80623785196e5a90a743114ebfc4cf7c1e873a8f
4ndradeGabriel <andradegabrielw@gmail.com>
2024-05-22 20:41:09 -0300

    update
    Improvement in the code, addition of the full date.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 const horas = document.getElementById('horas');
 const minutos = document.getElementById('minutos');
 const segundos = document.getElementById('segundos');
-//const date = document.getElementById('date');
+const data = document.getElementById('date');
 
 const relogio = setInterval(function time() {
     let dateToday = new Date();
@@ -10,12 +10,12 @@ const relogio = setInterval(function time() {
     let hr = dateToday.getHours().toString().padStart(2, '0');
     let min = dateToday.getMinutes().toString().padStart(2, '0');
     let sec = dateToday.getSeconds().toString().padStart(2, '0');
-    //let dat = dateToday.toLocaleDateString('pt-BR', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
+    let dat = dateToday.toLocaleDateString('pt-BR', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
 
     horas.textContent = hr;
     minutos.textContent = min;
     segundos.textContent = sec;
-    date.textContent = dat;
+    data.textContent = dat;
 
 
 })
